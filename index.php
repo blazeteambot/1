a3fec2a58adaf68312a3252f6e1a512882447ecf
BLAZETEAMBOT <34600028+blazeteambot@users.noreply.github.com>
2017-12-16 22:19:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if($message['type']=='sticker')
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
 $key = 'd5d28c19-67f0-4940-8156-91aa5355f1e4'; //API SimSimi
-$url = 'http://api.simsimi.com/keyword'.$pesan;
+$url = 'http://sandbox.api.simsimi.com/request.p'.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
 $diterima = $url['response'];
